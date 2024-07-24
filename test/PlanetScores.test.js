b591494fc26e257626097fb52bb776e55f17e737
Yilei Bai <marbink100@gmail.com>
2024-07-24 13:50:11 -0400

    renamed test file to be more accurate

---
+++ test/PlanetScores.test.js
@@ -0,0 +1,7 @@
+/**
+ * This file will be more advanced than the HandTypeChecker tests, as we are now factoring in hand types
+ * as well as the different scores they bring along with planet card "use".
+ * 
+ * These tests will verify that hand scores + planet card hand upgrades are calculated correctly.
+ */
+
