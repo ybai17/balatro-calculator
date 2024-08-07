651bc101a0361fe887bb709a0dd0d3514296731b
Yilei Bai <marbink100@gmail.com>
2024-08-07 16:00:00 -0400

    adding new class to be used in ScoreObject for handling RNG elements

---
+++ src/model/ScoreObjectRNGBundle.js
@@ -0,0 +1,13 @@
+/**
+ * This class defines the object that ScoreObject will accept to handle all the RNG aspects that affect
+ * scoring (e.g. LUCKY cards, RNG-based Jokers, etc.).
+ * 
+ * This object will be provided with a predetermined 
+ */
+class ScoreObjectRNGBundle {
+
+
+
+}
+
+export default ScoreObjectRNGBundle;
